bc5625ffd419dd2b372ec0732972a785e9b5f06f
TristanTrim <tristantrim@gmail.com>
2019-11-16 01:01:52 -0500

    Circles are getting cooler... but are still dum

--- liagw.js
+++ liagw.js
@@ -1,5 +1,4 @@
 
-
 //Fuck this shit eh!!!
 
 document.body.style.backgroundColor = "black";
@@ -12,13 +11,41 @@ document.body.style.backgroundColor = "black";
 function mouseMove(e){
     x = e.pageX;
     y = e.pageY;
-    moveCircle(x,y);
+    things[0][0]=x;
+    things[0][1]=y;
 }
 
+function hlProximalThing(){
+    let closest = 0;
+    for (i=1;i<things.length;i++){
+        //if(closest==0){
+            if(close(things[0],things[i])){
+                things[i][3]=true;
+            }else{
+                things[i][3]=false;
+            }
+        //}
+    }
+}
+
+function close(thing1,thing2){
+    x1=thing1[0];
+    y1=thing1[1];
+    x2=thing2[0];
+    y2=thing2[1];
+    r1=thing1[2];
+    r2=thing2[2];
+    return(Math.abs(x1-x2)**2+Math.abs(y1-y2)**2 < (r1+r2)**2);
+}
 var w = window.innerWidth;
 var h = window.innerHeight;
-
 var canvas = document.getElementById('canvas');
+
+var things = [];
+things.push([canvas.width/2,canvas.height/2,13],false);
+things.push([10,20,4,false]);
+things.push([20,20,4,false]);
+things.push([30,20,4,false]);
 
 fuck_you=20;
 canvas.width=window.innerWidth-fuck_you;
@@ -27,22 +54,33 @@ canvas.height=window.innerHeight-fuck_you;
 if (canvas.getContext)
 {
     var ctx = canvas.getContext('2d');
-    var X = canvas.width / 2;
-    var Y = canvas.height / 2;
-    var R = 13;
+}
+function clear(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+function drawCircle(coords){
+    x=coords[0];
+    y=coords[1];
+    r=coords[2];
     ctx.beginPath();
-    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    ctx.arc(x, y, r, 0, 2 * Math.PI, false);
     ctx.lineWidth = 1;
-    ctx.strokeStyle = '#00FF00';
+    if(coords[3]){
+        ctx.strokeStyle = '#DDFFFF';
+    }else{
+        ctx.strokeStyle = '#00FF00';
+    }
     ctx.stroke();
 }
 
-function moveCircle(x,y){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.arc(x, y, R, 0, 2 * Math.PI, false);
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = '#00FF00';
-    ctx.stroke();
+// This should be based on activity, rather than constant, but... yeah.
+function tic(){
+    hlProximalThing();
+    clear();
+    for(i=0;i<things.length;i++){
+        drawCircle(things[i]);
+    }
+    setTimeout(tic,39);
 }
+tic();
 
